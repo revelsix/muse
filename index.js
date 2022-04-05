effc8d3f4557723c4d9475c3d23d55b000fc2f02
revelsix <45189851+revelsix@users.noreply.github.com>
2022-04-05 11:11:38 -0400

    jgagooagij

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 var uahgh = new Audio('./uahgh.mp3');
+var uahgh1 = false;
+
+var counter = 0;
+
+function main(){
+    counter++;
+    if(counter = 4){
+        playAudio();
+        counter = 0;
+    }
+}
+
+function toggleAudio(name){
+    if(name){
+        name = false;
+    } else {
+        name = true;
+    }
+}
+
 function playAudio(){
-    uahgh.play();
+    if(uahgh1){
+        uahgh.play();
+    }
 }
