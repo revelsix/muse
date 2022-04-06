fc4155249ebe88cef0bcd26acc078f8d440c4ac2
revelsix <45189851+revelsix@users.noreply.github.com>
2022-04-06 12:23:04 -0400

    fixed?????????

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 var uahgh = new Audio('./uahgh.mp3');
 var uahgh1 = false;
 
-var counter = 0;
+let counter = 0;
 
 function main(){
-    console.log(counter);
+    
     counter++;
-    if(counter = 4){
+    if(counter == 4){
         playAudio();
+        console.log('audio played');
         counter = 0;
     }
+    console.log(counter);
 }
 
 function toggleAudio(name){
     console.log('Toggled ' + name);
-    if(name){
-        name = false;
+    if(name == uahgh1 && uahgh1 == true){
+        uahgh1 = false;
     } else {
-        name = true;
+        uahgh1 = true;
     }
-    console.log(name);
-    console.log(uahgh);
+    console.log(name + ' name');
+    console.log(uahgh1 + ' uahgh1');
 }
 
 function playAudio(){
